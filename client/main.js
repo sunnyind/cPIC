fc0192e0145d06e7c91f1ab026da707694364f4d
alpaslan <alpaslan@mi.fu-berlin.de>
2017-06-11 15:53:52 +0200

    geändert:       client/main.js 	geändert:       imports/pages/getStarted.html 	geändert:       imports/pages/group.html 	geändert:       imports/pages/userList.html

--- client/main.js
+++ client/main.js
@@ -16,21 +16,57 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 
 Meteor.subscribe('userStatus')
 
+//ein paar allgemeine Funktionen
+
+function f_meineGruppe (){
+	return Gruppen.findOne({"nutzer": Meteor.user().username}).Gruppe;
+}
+
+function f_seineGruppe (x){
+	return Gruppen.findOne({"nutzer": x}).Gruppe
+}
+
+
+
 Template.body.helpers({
 	actual_user(){
 		return Meteor.user().username;
 	}
 })
 
-Template.userList.({
+Template.userList.helpers({
   usersOnline:function(){
     return Meteor.users.find({ "status.online": true , "username":{$ne: Meteor.user().username}})
   },
- 
+
+  inmygroup:function(x){
+	var y = f_seineGruppe(x);
+	var y2 = f_meineGruppe();
+  	return y==y2
+  },
+
+  'submit .user_online_einladen'(event){
+	const nameeinladen = event.target.sub.value;
+	const test = target.submit.value;
+	var now = f_meineGruppe();
+		//Gruppen.find({"nutzer": Meteor.user().username})
+	Gruppen.update(
+		{"nutzer": nameeinladen},
+		{$set: {"Gruppe": now}}
+	);
+
+   }
 })
 
 Template.group.helpers({
-	
+	groupname:function(){
+		 return Gruppen.find({"nutzer": Meteor.user().username},{nutzer:0, _id:0} );	 
+	},
+
+	notgroupmember:function(){
+		return Meteor.users.find({ "status.online": true , "username":{$ne: Meteor.user().username}})
+	}
+
 })
 
 Template.newgrouptemp.events({
@@ -48,5 +84,22 @@ Template.newgrouptemp.events({
 
 		target.text.value ='';
 
+	}
+})
+
+Template.newgrouptemp.helpers({
+	
+
+	ingroup:function(){
+		return Gruppen.find({"nutzer": {$in: [Meteor.user().username]}  }).count()>0
+	},
+
+	groupname:function(){
+		return f_meineGruppe();
+	},
+
+	groupmember:function(){
+		var x = f_meineGruppe();
+		return Gruppen.find({"Gruppe": x})
 	}
 })
